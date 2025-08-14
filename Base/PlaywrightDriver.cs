9b1946cb01a66b9b44a768a3abc193d673e2a698
yanivbd7455 <yanivbd@hotmail.com>
2025-08-14 22:49:52 +0300

    Init playwright driver manager

---
+++ Base/PlaywrightDriver.cs
@@ -0,0 +1,103 @@
+using Genpact.Conf;
+using Microsoft.Playwright;
+using NLog;
+
+
+namespace Genpact.Base;
+
+public class PlaywrightDriver
+{
+    private static readonly Logger logger = LogManager.GetCurrentClassLogger();
+    private static IPlaywright? playwrightInstance = null;
+    private static readonly object _lock = new object();
+
+
+    public async static Task<(IBrowser browser, IPage page, IBrowserContext browserContext, IPlaywright playwrightInstance)> InitializePlaywrightAsync()
+    {
+        playwrightInstance = ManagePlaywrightSingletone();    
+        var browserInstance = await InitBrowserAsync(playwrightInstance);
+        var browserContext = await InitBrowserContextNoTokenAsync(browserInstance);
+        var page = await browserContext.NewPageAsync();
+        var apiRequestContext = browserContext.APIRequest;
+        return (browserInstance, page, browserContext, playwrightInstance);
+    }
+
+    
+    public static IPlaywright ManagePlaywrightSingletone()
+    {
+        lock (_lock)
+        {
+            playwrightInstance = playwrightInstance ?? Playwright.CreateAsync().Result;
+        }
+        return playwrightInstance;
+    }
+
+    public async static Task<IBrowser> InitBrowserAsync(IPlaywright playwright)
+    {
+        string browserType = "Chromium";
+        BrowserTypeLaunchOptions launchOptions = new BrowserTypeLaunchOptions { Headless = false, SlowMo = 50 };
+        return browserType switch
+        {
+            "Chromium" => await playwright.Chromium.LaunchAsync(launchOptions),
+            "Chrome" => await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                ExecutablePath = @"C:\Program Files\Google\Chrome\Application\chrome.exe",
+                Headless = false
+            }),
+            "Firefox" => await playwright.Firefox.LaunchAsync(launchOptions),
+            "Webkit" => await playwright.Webkit.LaunchAsync(launchOptions),
+            _ => await playwright.Chromium.LaunchAsync(launchOptions)
+        };
+    }
+
+    public async static Task<IBrowserContext> InitBrowserContextNoTokenAsync(IBrowser browser)
+    {
+        var context = await browser.NewContextAsync(new BrowserNewContextOptions
+        {
+            BaseURL = Config.WikiWebBaseUrl,
+            IgnoreHTTPSErrors = true,
+            ExtraHTTPHeaders = new Dictionary<string, string>
+            {
+                {"Content-Type","application/x-www-form-urlencoded; charset=utf-8"},
+            }
+        });
+        #region BrowserNewContextOptions
+        /*          AcceptDownloads = clone.AcceptDownloads;
+                    BaseURL = clone.BaseURL;
+                    BypassCSP = clone.BypassCSP;
+                    ColorScheme = clone.ColorScheme;
+                    DeviceScaleFactor = clone.DeviceScaleFactor;
+                    ExtraHTTPHeaders = clone.ExtraHTTPHeaders;
+                    ForcedColors = clone.ForcedColors;
+                    Geolocation = clone.Geolocation;
+                    HasTouch = clone.HasTouch;
+                    HttpCredentials = clone.HttpCredentials;
+                    IgnoreHTTPSErrors = clone.IgnoreHTTPSErrors;
+                    IsMobile = clone.IsMobile;
+                    JavaScriptEnabled = clone.JavaScriptEnabled;
+                    Locale = clone.Locale;
+                    Offline = clone.Offline;
+                    Permissions = clone.Permissions;
+                    Proxy = clone.Proxy;
+                    RecordHarContent = clone.RecordHarContent;
+                    RecordHarMode = clone.RecordHarMode;
+                    RecordHarOmitContent = clone.RecordHarOmitContent;
+                    RecordHarPath = clone.RecordHarPath;
+                    RecordHarUrlFilter = clone.RecordHarUrlFilter;
+                    RecordHarUrlFilterRegex = clone.RecordHarUrlFilterRegex;
+                    RecordHarUrlFilterString = clone.RecordHarUrlFilterString;
+                    RecordVideoDir = clone.RecordVideoDir;
+                    RecordVideoSize = clone.RecordVideoSize;
+                    ReducedMotion = clone.ReducedMotion;
+                    ScreenSize = clone.ScreenSize;
+                    ServiceWorkers = clone.ServiceWorkers;
+                    StorageState = clone.StorageState;
+                    StorageStatePath = clone.StorageStatePath;
+                    StrictSelectors = clone.StrictSelectors;
+                    TimezoneId = clone.TimezoneId;
+                    UserAgent = clone.UserAgent;
+                    ViewportSize = clone.ViewportSize;*/
+        #endregion
+        return context;
+    }
+}
