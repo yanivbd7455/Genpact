916616756f96ad45924cf00abb7ae1faf7ab0689
yanivbd7455 <yanivbd@hotmail.com>
2025-08-15 09:12:52 +0300

    Add Locators

--- Locators/WikiPageLocators.cs
+++ Locators/WikiPageLocators.cs
@@ -12,5 +12,11 @@ public class WikiPageLocators
         _page = page;
     }
 
-    public ILocator DebuggingFeatures => _page.Locator("//input[@id='username']");
+    public ILocator PlaywrightSoftwareHeading => _page.GetByRole(AriaRole.Heading, new() { Name = "Playwright (software)" }).Locator("span");
+    public ILocator DebuggingFeaturesLink => _page.GetByRole(AriaRole.Link, new() { Name = "Debugging features" });
+    public ILocator DebuggingFeaturesHeading => _page.GetByRole(AriaRole.Heading, new() { Name = "Debugging features" });
+    public ILocator DebuggingFeaturesSubHeading => _page.GetByText("Playwright includes built-in");
+    public ILocator DebuggingFeaturesBuiltInArray => _page.Locator("//*[@id='mw-content-text']/div[1]/ul[2]");
+    public ILocator DebuggingFeaturesBuiltInArrayCss => _page.Locator("#mw-content-text > div.mw-content-ltr.mw-parser-output > ul:nth-child(27)");
+
 }
