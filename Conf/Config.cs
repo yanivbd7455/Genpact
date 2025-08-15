5824b66238b317f7f5e7783841104b44c9d39361
yanivbd7455 <yanivbd@hotmail.com>
2025-08-15 10:22:27 +0300

    Add MediaWikiBaseUrl param

--- Conf/Config.cs
+++ Conf/Config.cs
@@ -3,5 +3,6 @@ namespace Genpact.Conf
     public static class Config
     {
         public static readonly string? WikiWebBaseUrl = ConfigManager.GetPropertyValue("wikiWebBaseUrl");
+        public static readonly string? MediaWikiBaseUrl = ConfigManager.GetPropertyValue("mediaWikiBaseUrl");
     }
 }
